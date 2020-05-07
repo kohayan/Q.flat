cfc1cd3e8d66344234351d4b0114e8b4bd90297f
kohayan <disney.2381509@gmail.com>
2020-05-07 06:04:27 +0000

    fix model/Notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,7 +6,7 @@ class Notification < ApplicationRecord
 	belongs_to :visitor, class_name: "User", optional: true
 	belongs_to :visited, class_name: "User", optional: true
 	belongs_to :quiz, optional: true
-	belongs_to :comment, optional: true
+	belongs_to :quiz_comment, optional: true
 
 	default_scope -> { order(created_at: :desc) }
 end
