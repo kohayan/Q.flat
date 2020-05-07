1beaa349c7aaadb15a01ed3b3b9bf47c90490b4c
kohayan <disney.2381509@gmail.com>
2020-05-07 08:48:05 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'homes/top'
-  get 'homes/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	devise_for :users
+
+	root 'homes#top'
+	get 'homes/about'
+
+	resources :users,only: [:show,:edit,:update,:index] do
+		resource :relationships, only: [:create, :destroy]
+		get 'follows' => 'relationships#follower', as: 'follows'
+		get 'followers' => 'relationships#followed', as: 'followers'
+	end
+
+	resources :quizzes do
+		resource :favorites, only:[:create, :destroy]
+		resource :quiz_comments, only:[:create, :destroy]
+	end
+
+	resources :notifications, only: [:index]
 end
