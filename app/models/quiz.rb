2f87014c51ab2cf46b4e48c62282c4275934018a
kohayan <disney.2381509@gmail.com>
2020-05-09 13:15:58 +0000

    fix view quiz/index,show

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -6,4 +6,8 @@ class Quiz < ApplicationRecord
 	has_many :quiz_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 	has_many :categories, through: :quiz_categories
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
 end
