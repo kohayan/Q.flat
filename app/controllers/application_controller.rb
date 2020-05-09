44f2e14dfdf1adc30ad8738b15e12b0609bcf865
kohayan <disney.2381509@gmail.com>
2020-05-09 05:55:31 +0000

    fix controller/quizzes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
-
+	before_action :new_quiz
 
 	protected
+
+	def new_quiz
+		@new_quiz = Quiz.new
+	end
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :email])
