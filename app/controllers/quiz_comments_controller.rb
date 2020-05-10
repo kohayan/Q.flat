dfcf0dd3d4dd29c01b03bb58fb1191d44c7a0797
kohayan <disney.2381509@gmail.com>
2020-05-10 05:59:36 +0000

    add quiz_comments#create,destroy

--- app/controllers/quiz_comments_controller.rb
+++ app/controllers/quiz_comments_controller.rb
@@ -1,2 +1,33 @@
 class QuizCommentsController < ApplicationController
+	before_action :set_quiz
+
+	def create
+		@comment = @quiz.quiz_comments.new(quiz_comment_params)
+		@comment.user_id = current_user.id
+		if @comment.save
+			flash[:success] = "コメントを送信しました"
+		else
+			redirect_back(fallback_location: root_path)
+		end
+	end
+
+	def destroy
+		@comment = QuizComment.find(params[:id])
+		if @comment.user_id != current_user.id
+			redirect_back(fallback_location: root_path)
+		end
+		@comment.destroy
+	end
+
+
+	private
+
+	def set_quiz
+		@quiz = Quiz.find(params[:quiz_id])
+	end
+
+	def quiz_comment_params
+		params.require(:quiz_comment).permit(:comment)
+	end
+
 end
