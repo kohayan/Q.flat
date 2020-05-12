eab08d3217894d27d65f52e875f0cf329dcfc592
kohayan <disney.2381509@gmail.com>
2020-05-12 08:10:20 +0000

    fix models

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,12 @@ class User < ApplicationRecord
 	def following?(user)
 		following_user.include?(user)
 	end
+
+	def create_notification_follow!(current_user)
+		notification = current_user.active_notifications.new(
+			visited_id: id,
+			action: 'follow'
+		)
+		notification.save if notification.valid?
+	end
 end
