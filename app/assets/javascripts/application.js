9d71954ba7914c3711b8a48571223607e32ab72d
kohayan <disney.2381509@gmail.com>
2020-05-13 06:45:13 +0000

    fix quiz_form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -83,6 +83,44 @@ $(document).on('turbolinks:load',function() {
 		$('.quiz-form-wrapper').fadeOut();
 	});
 
+	// クイズの投稿ボタンの有効、無効の切り替え
+	$('.quiz-send').prop("disabled", true);
+
+	$('.quiz-form').on('input', function(){
+		//文字数を取得
+		var q_cnt = $('.question-area').val().length;
+		var a_cnt = $('.answer-area').val().length;
+		var e_cnt = $('.explanation-area').val().length;
+		//現在の文字数を表示
+		$('.question_now_cnt').text(q_cnt);
+		$('.answer_now_cnt').text(a_cnt);
+		$('.explanation_now_cnt').text(e_cnt);
+
+		// 各項目の文字数によってnow_cntの色を変更
+		if(q_cnt > 0 && 501 > q_cnt){
+			$('.question_cnt_area').removeClass('cnt_danger');
+		}else{
+			$('.question_cnt_area').addClass('cnt_danger');
+		}
+		if(a_cnt > 0 && 201 > a_cnt){
+			$('.answer_cnt_area').removeClass('cnt_danger');
+		}else{
+			$('.answer_cnt_area').addClass('cnt_danger');
+		}
+		if(501 > e_cnt){
+			$('.explanation_cnt_area').removeClass('cnt_danger');
+		}else{
+			$('.explanation_cnt_area').addClass('cnt_danger');
+		}
+
+		// 投稿ボタンの有効、無効の切り替え
+		if(!$('.question_cnt_area').hasClass('cnt_danger') && !$('.answer_cnt_area').hasClass('cnt_danger') && !$('.explanation_cnt_area').hasClass('cnt_danger')) {
+			$('.quiz-send').prop("disabled", false);
+		}else{
+			$('.quiz-send').prop("disabled", true);
+		}
+	});
+
 	// クイズ詳細画面の答え、解説をアコーディオン表示
 	$('.answer-show').click(function() {
 		if($('.answer').hasClass('open')) {
@@ -118,23 +156,22 @@ $(document).on('turbolinks:load',function() {
 	});
 
 
-	// コメントの送信ボタンを有効、無効の切り替え
+	// コメントの送信ボタンの有効、無効の切り替え
 	$('.comment-send').prop("disabled", true);
 
 	$('.comment-area').on('input', function(){
-        //文字数を取得
-        var cnt = $(this).val().length;
-        //現在の文字数を表示
-        $('.now_cnt').text(cnt);
-        if(cnt > 0 && 141 > cnt){
-            //1文字以上かつ140文字以内の場合はボタンを有効化＆黒字
-            $('.comment-send').prop('disabled', false);
-            $('.cnt_area').removeClass('cnt_danger');
-        }else{
-            //0文字または140文字を超える場合はボタンを無効化＆赤字
-            $('.comment-send').prop('disabled', true);
-            $('.cnt_area').addClass('cnt_danger');
-        }
-    });
-
+		//文字数を取得
+		var cnt = $(this).val().length;
+		//現在の文字数を表示
+		$('.now_cnt').text(cnt);
+		if(cnt > 0 && 141 > cnt){
+			//1文字以上かつ140文字以内の場合はボタンを有効化＆黒字
+			$('.comment-send').prop('disabled', false);
+			$('.cnt_area').removeClass('cnt_danger');
+		}else{
+			//0文字または140文字を超える場合はボタンを無効化＆赤字
+			$('.comment-send').prop('disabled', true);
+			$('.cnt_area').addClass('cnt_danger');
+		}
+	});
 });
