d7dbfd1610deefb2ffb97d7c669e22e620c8e1b7
kohayan <disney.2381509@gmail.com>
2020-05-21 06:08:44 +0000

    fix controller registrations, sessions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,13 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    if user_signed_in?
-      flash[:notice] = "ログインしています"
-      redirect_to :root
-    end
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource
   # def create
