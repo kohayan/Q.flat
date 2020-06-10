51ed81b2a5657a3b658c7117fa6aa336b7398670
kohayan <disney.2381509@gmail.com>
2020-06-10 12:35:33 +0000

    fix spec/requests/homes,quizzes,search

--- spec/requests/homes_request_spec.rb
+++ spec/requests/homes_request_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /top" do
-    it "returns http success" do
-      get "/homes/top"
-      expect(response).to have_http_status(:success)
+  describe 'アバウトページ' do
+        context "アバウトページが正しく表示される" do
+            before do
+                get homes_about_path
+            end
+            it 'リクエストは200 OKとなること' do
+                expect(response.status).to eq 200
+            end
+        end
     end
-  end
-
-  describe "GET /about" do
-    it "returns http success" do
-      get "/homes/about"
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
--- spec/requests/quizzes_request_spec.rb
+++ spec/requests/quizzes_request_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Quizzes", type: :request do
+	describe 'クイズ一覧ページ' do
+        context "クイズ一覧ページが正しく表示される" do
+            before do
+                get quizzes_path
+            end
+            it 'リクエストは200 OKとなること' do
+                expect(response.status).to eq 200
+            end
+        end
+    end
+
+    describe '人気クイズページ' do
+        context "人気クイズページが正しく表示される" do
+            before do
+                get famous_quizzes_path
+            end
+            it 'リクエストは200 OKとなること' do
+                expect(response.status).to eq 200
+            end
+        end
+    end
 end
