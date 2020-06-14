3f884cf1d9289f39c580a66b283e6ac1fa2a7faa
kohayan <disney.2381509@gmail.com>
2020-06-14 12:27:09 +0000

    fix spec/requests/quizzes, add test_edit

--- spec/requests/quizzes_request_spec.rb
+++ spec/requests/quizzes_request_spec.rb
@@ -31,6 +31,34 @@ RSpec.describe "Quizzes", type: :request do
         end
     end
 
+    describe 'GET #edit' do
+        subject { get edit_quiz_path(quiz) }
+        context 'ログインしている場合' do
+            before do
+                sign_in user
+            end
+            it 'リクエストが成功すること' do
+                is_expected.to eq 200
+            end
+        end
+
+        context 'クイズが存在しない場合' do
+            before do
+                sign_in user
+            end
+            it 'リダイレクトされること' do
+                quiz.destroy
+                is_expected.to redirect_to quizzes_path
+            end
+        end
+
+        context 'ゲストの場合' do
+            it 'リダイレクトされること' do
+                is_expected.to redirect_to new_user_session_path
+            end
+        end
+    end
+
     describe '人気クイズページ' do
         context "人気クイズページが正しく表示される" do
             before do
