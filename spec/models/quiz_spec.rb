ec2ebef523bf68d8d8ed42a97dc07ea15b174e45
kohayan <disney.2381509@gmail.com>
2020-06-15 14:59:45 +0000

    fix spec/models/quiz

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:user) { create(:user) }
+    let(:quiz) { create(:quiz, user_id: user.id) }
+    let(:quiz_params) { attributes_for(:quiz) }
+    let(:invalid_quiz_params) { attributes_for(:quiz, question: "") }
+
+  	context "データが正しく保存される" do
+        before do
+            @quiz = user.quizzes.new(quiz_params)
+            @quiz.save
+        end
+        it "全て入力してあるので保存される" do
+            expect(@quiz).to be_valid
+        end
+    end
 end
