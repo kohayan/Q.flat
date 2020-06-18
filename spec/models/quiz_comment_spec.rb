0589dc359bcc4a4baf2df48cad477c463389a0f1
kohayan <disney.2381509@gmail.com>
2020-06-18 13:34:18 +0000

    fix spec/models/quiz_comment

--- spec/models/quiz_comment_spec.rb
+++ spec/models/quiz_comment_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe QuizComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:user) { create(:user) }
+	let(:quiz) { create(:quiz, user_id: user.id) }
+
+	context "コメントが正しく保存される" do
+        before do
+            @comment = quiz.quiz_comments.new(user_id: user.id, comment: "テスト")
+            @comment.save
+        end
+        it "全て入力してあるので保存される" do
+            expect(@comment).to be_valid
+        end
+    end
+
+    context "コメントが正しく保存されない" do
+        before do
+            @comment = quiz.quiz_comments.new(user_id: user.id, comment: "")
+            @comment.save
+        end
+        it "commentが入力されていないので保存されない" do
+            expect(@comment).to be_invalid
+        end
+    end
+
+    context "コメントが正しく削除される" do
+        before do
+            @comment = quiz.quiz_comments.new(user_id: user.id, comment: "テスト")
+            @comment.save
+            @comment.destroy
+        end
+        it "正しく削除される" do
+            expect(@comment).to be_valid
+        end
+    end
 end
