76ee19db6d0cdf4adf9400e705044fd9e8d986f1
kohayan <disney.2381509@gmail.com>
2020-06-18 13:45:23 +0000

    fix spec/models/favorite

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:user) { create(:user) }
+	let(:quiz) { create(:quiz, user_id: user.id) }
+
+	context "お気に入りが正しく保存される" do
+        before do
+            @favorite = quiz.favorites.new(user_id: user.id)
+			@favorite.save
+        end
+        it "保存される" do
+            expect(@favorite).to be_valid
+        end
+    end
+
+    context "お気に入りが正しく保存されない" do
+        before do
+            @favorite = Favorite.new(user_id: "", quiz_id: "")
+			@favorite.save
+        end
+        it "保存されない" do
+            expect(@favorite).to be_invalid
+        end
+    end
 end
