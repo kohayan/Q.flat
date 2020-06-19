9f85a038718962aa7b756280f2c480066d42dfbc
kohayan <disney.2381509@gmail.com>
2020-06-19 13:54:04 +0000

    fix spec/factories/users.rb, spec/models/relationship

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,4 +4,10 @@ FactoryBot.define do
     sequence(:email) { |n| "TEST#{n}@example.com" }
     password { "testuser" }
   end
+
+  factory :other_user, class: User do
+    nick_name { "other_test" }
+    sequence(:email) { |n| "OTHER#{n}@example.com" }
+    password { "otheruser" }
+  end
 end
