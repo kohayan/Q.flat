168a6de7da02a462118862fc6b82bc6111bd961f
kohayan <disney.2381509@gmail.com>
2020-06-20 12:37:21 +0000

    fix spec/models/tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	context "タグが正しく保存される" do
+        before do
+            @tag = Tag.new(name: "tag_test")
+            @tag.save
+        end
+        it "正しく保存される" do
+            expect(@tag).to be_valid
+        end
+    end
+
+    context "タグが正しく保存されない" do
+        before do
+            @tag = Tag.new(name: "")
+            @tag.save
+        end
+        it "正しく保存されない" do
+            expect(@tag).to be_invalid
+        end
+    end
+
+    context "タグが正しく削除される" do
+        before do
+            @tag = Tag.new(name: "tag_test")
+            @tag.save
+            @tag.destroy
+        end
+        it "正しく削除される" do
+            expect(@tag).to be_valid
+        end
+    end
 end
