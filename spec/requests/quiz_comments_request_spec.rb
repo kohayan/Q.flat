465a30b16f02e68e091e0422b1e27992e8415291
kohayan <disney.2381509@gmail.com>
2020-06-25 14:55:27 +0000

    fix  param is missing or the value is empty

--- spec/requests/quiz_comments_request_spec.rb
+++ spec/requests/quiz_comments_request_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "QuizComments", type: :request do
+	let(:user) { create(:user) }
+	let(:other_user) { create(:other_user) }
+	let(:quiz) { create(:quiz, user_id: user.id) }
+
+	describe 'POST #create' do
+		before do
+			sign_in user
+			comment = QuizComment.new
+		end
+
+		context 'パラメータが妥当な場合' do
+			it 'リクエストが成功すること' do
+				post quiz_quiz_comments_path(quiz, comment), params: { comment: "test-comment" }
+				expect(response.status).to eq 302
+			end
+
+			it 'createが成功すること' do
+				expect do
+					post quiz_quiz_comments_path(quiz, comment), params: { comment: "test-comment" }
+				end
+			end
+		end
+
+		context 'パラメータが不正な場合' do
+			it 'リクエストが成功すること' do
+				post quiz_quiz_comments_path(quiz, comment), params: { comment: "" }
+				expect(response.status).to eq 200
+			end
+
+			it 'createが失敗すること' do
+				expect do
+					post quiz_quiz_comments_path(quiz, comment), params: { comment: "" }
+				end
+			end
+		end
+	end
 end
