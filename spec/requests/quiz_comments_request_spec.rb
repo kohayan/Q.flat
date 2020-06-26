769d020ca90b2dd090d5281403614001503d35a7
kohayan <disney.2381509@gmail.com>
2020-06-26 01:47:10 +0000

    fix spec/requests/quiz_comments

--- spec/requests/quiz_comments_request_spec.rb
+++ spec/requests/quiz_comments_request_spec.rb
@@ -1,40 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "QuizComments", type: :request do
-	let(:user) { create(:user) }
-	let(:other_user) { create(:other_user) }
-	let(:quiz) { create(:quiz, user_id: user.id) }
+  let(:user) { create(:user) }
+  let(:other_user) { create(:other_user) }
+  let(:quiz) { create(:quiz, user_id: user.id) }
 
-	describe 'POST #create' do
-		before do
-			sign_in user
-			comment = QuizComment.new
-		end
+  describe 'POST #create' do
+    before do
+      sign_in user
+    end
 
-		context 'パラメータが妥当な場合' do
-			it 'リクエストが成功すること' do
-				post quiz_quiz_comments_path(quiz, comment), params: { comment: "test-comment" }
-				expect(response.status).to eq 302
-			end
+    context 'パラメータが妥当な場合' do
+      it 'リクエストが成功すること' do
+        post quiz_quiz_comments_path(quiz), params: { quiz_comment: { comment: "test-comment" } }, xhr: true
+        expect(response.status).to eq 200
+      end
 
-			it 'createが成功すること' do
-				expect do
-					post quiz_quiz_comments_path(quiz, comment), params: { comment: "test-comment" }
-				end
-			end
-		end
+      it 'createが成功すること' do
+        expect do
+          post quiz_quiz_comments_path(quiz), params: { quiz_comment: { comment: "test-comment" } }, xhr: true
+        end
+      end
+    end
 
-		context 'パラメータが不正な場合' do
-			it 'リクエストが成功すること' do
-				post quiz_quiz_comments_path(quiz, comment), params: { comment: "" }
-				expect(response.status).to eq 200
-			end
+    context 'パラメータが不正な場合' do
+      it 'リクエストが成功すること' do
+        post quiz_quiz_comments_path(quiz), params: { quiz_comment: { comment: "" } }, xhr: true
+        expect(response.status).to eq 200
+      end
 
-			it 'createが失敗すること' do
-				expect do
-					post quiz_quiz_comments_path(quiz, comment), params: { comment: "" }
-				end
-			end
-		end
-	end
+      it 'createが失敗すること' do
+        expect do
+          post quiz_quiz_comments_path(quiz), params: { quiz_comment: { comment: "" } }, xhr: true
+        end
+      end
+    end
+  end
 end
